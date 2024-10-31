adee63e4f157a4d5f398daa1c20fc0d8779f9482
muhammaddiaz13 <willyambevan@gmail.com>
2024-10-31 22:45:29 +0700

    soal_3.pas

--- coding/soal_3.pas
+++ coding/soal_3.pas
@@ -1,1 +1,22 @@
+program segitiga_biner;
+uses crt;
 
+var
+    i, j, tinggi: integer;
+
+begin
+    clrscr;
+    write('Tinggi segitiga : ');
+    readln(tinggi);
+    for i:=1 to tinggi do
+    begin
+        for j:=1 to i do
+        begin
+            if j mod 2 = 1 then
+                write('1')
+            else
+                write('0');
+        end;
+        writeln;
+    end;
+end.
