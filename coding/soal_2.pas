197205ada416d0ca4e719007e26c4ad7c5bf225c
muhammaddiaz13 <willyambevan@gmail.com>
2024-10-31 22:44:50 +0700

    soal_2.pas

--- coding/soal_2.pas
+++ coding/soal_2.pas
@@ -1,1 +1,26 @@
+program hitung_angka;
+uses crt;
 
+var
+n, i, angka: integer;
+total, rata_rata: real;
+
+begin
+    clrscr;
+    write('N : '); readln(n);
+
+    total := 0;
+    rata_rata := 0;
+
+    for i := 1 to n do 
+    begin        
+    write('Angka ke-',i,': '); readln(angka);
+    total := total + angka;
+    end;
+    
+    rata_rata := total / n;
+
+    writeln('Jumlah Total : ',total:0:0);
+    write('Jumlah Rata-rata : ',rata_rata:0:2);
+    readln;
+end.
